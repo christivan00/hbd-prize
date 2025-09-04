4d855054ebec164c7d86b3d7a90d9d1d3f3514da
christivan00 <157097188+christivan00@users.noreply.github.com>
2025-09-04 15:08:58 +0700

    last

---
+++ js/script.js
@@ -0,0 +1,31 @@
+function openSurprise() {
+        // Sembunyiin overlay
+        document.getElementById("overlay").style.display = "none";
+        // Tampilkan konten utama
+        document.getElementById("main-content").style.display = "flex";
+        // Play musik
+        document.getElementById("bg-music").play();
+
+        // Efek confetti
+        let duration = 3 * 1000; // 3 detik
+        let end = Date.now() + duration;
+
+        (function frame() {
+            confetti({
+            particleCount: 7,
+            angle: 60,
+            spread: 55,
+            origin: { x: 0 }
+            });
+            confetti({
+            particleCount: 7,
+            angle: 120,
+            spread: 55,
+            origin: { x: 1 }
+            });
+
+            if (Date.now() < end) {
+            requestAnimationFrame(frame);
+            }
+        }());
+        }
